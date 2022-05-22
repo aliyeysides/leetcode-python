31378bfc66c70369808eda75b5579b50fe204169
Ali Yeysides <ali.yeysides@gmail.com>
2022-05-22 11:38:25 -0400

    adds recursive gen paren solution

--- test.py
+++ test.py
@@ -123,3 +123,16 @@ class Solution:
                 return False
 
         return len(stack) == 0
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def generate(p, left, right, parens=[]):
+            if left:
+                generate(p + '(', left-1, right)
+            if right > left:
+                generate(p + ')', left, right-1)
+            if not right:
+                parens += p,
+            return parens
+        return generate('', n, n)
