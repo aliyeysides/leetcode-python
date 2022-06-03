761a0cacda830f00ede5829710a71f5f67030e26
Ali Yeysides <ali.yeysides@gmail.com>
2022-06-03 10:12:14 -0400

    adds graph class

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Optional
 
 
@@ -210,3 +211,42 @@ class Solution:
                     queue.append(edge)
                     visited.append(edge)
         return res
+
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, vertex, edge):
+        self.graph[vertex].append(edge)
+        print(self.graph)
+
+    def BFS(self, s):
+        visited = []
+        queue = []
+        res = []
+
+        queue.append(s)
+        visited.append(s)
+
+        while queue:
+            n = queue.pop(0)
+            res.append(n)
+            for edge in self.graph[n]:
+                if edge not in visited:
+                    visited.append(edge)
+                    queue.append(edge)
+
+        return res
+
+
+g = Graph()
+
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+# print(g.BFS(2))
