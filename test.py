00e30c7178bc44b889590eb56f3fc088a4ae9164
Ali Yeysides <ali.yeysides@gmail.com>
2022-06-03 11:48:12 -0400

    adds bfs level order tree

--- test.py
+++ test.py
@@ -219,7 +219,6 @@ class Graph:
 
     def addEdge(self, vertex, edge):
         self.graph[vertex].append(edge)
-        print(self.graph)
 
     def BFS(self, s):
         visited = []
@@ -250,3 +249,33 @@ g.addEdge(2, 3)
 g.addEdge(3, 3)
 
 # print(g.BFS(2))
+
+
+class Node:
+    def __init__(self, v):
+        self.val = v
+        self.left = None
+        self.right = None
+
+    def levelOrder(self, root):
+        res = []
+        queue = []
+        queue.append(root)
+
+        while queue:
+            node = queue.pop(0)
+            res.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return res
+
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+# print(root.levelOrder(root))
