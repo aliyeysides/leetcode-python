1d041969954c38494a86b1c5630bb047b3757ce4
Ali Yeysides <ali.yeysides@gmail.com>
2022-06-03 14:56:18 -0400

    adds dfs for graph

--- test.py
+++ test.py
@@ -282,3 +282,23 @@ root.left.left = Node(4)
 root.left.right = Node(5)
 
 # print(root.levelOrder(root))
+
+
+class DFSGraph:
+    def dfsOfGraph(self, V, adj):
+        visit, result = [], []
+
+        def dfs(node, visit, result):
+            result.append(node)
+            visit.append(node)
+            for i in adj[node]:
+                if i not in visit:
+                    dfs(i, visit, result)
+
+        dfs(0, visit, result)
+
+        return result
+
+
+dfsG = DFSGraph()
+# print(dfsG.dfsOfGraph(5, {0: [1, 2, 4], 1: [], 2: [], 3: [], 4: [3]}))
