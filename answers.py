b8e5e9f420076b233467e8dbf21f51b84990317c
Ali Yeysides <ali.yeysides@gmail.com>
2022-06-05 19:40:53 -0400

    adds 3sum solution

--- answers.py
+++ answers.py
@@ -206,3 +206,32 @@ class Solution:
                 r -= 1
 
         return record
+
+
+class Solution:
+    def threeSum(self, nums: list[int]):
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                sum = nums[i] + nums[l] + nums[r]
+                if sum < 0:
+                    l += 1
+                elif sum > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return res
+
+
+s = Solution()
+print(s.threeSum([-1, 0, 1, 2, -1, -4]))
