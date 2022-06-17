18981814a5fbf789dc70405c3495504af06b8793
Ali Yeysides <ali.yeysides@gmail.com>
2022-06-17 14:13:13 -0400

    adds merge intervals solution

--- answers.py
+++ answers.py
@@ -1,4 +1,3 @@
-import heapq
 from typing import Optional
 
 
@@ -448,3 +447,14 @@ class Solution:
             return 1
 
         return self.ans + 1 if dfs(root) > 2 else self.ans
+
+
+class Solution:
+    def merge(self, intervals: list[list[int]]) -> list[list[int]]:
+        out = []
+        for i in sorted(intervals, key=lambda i: i[0]):
+            if out and i[0] <= out[-1][1]:
+                out[-1][1] = max(out[-1][1], i[1])
+            else:
+                out.append(i)
+        return out
