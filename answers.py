2d8ba24e9172421bdb978bc6426ccf1bb75d0595
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-05 13:20:37 -0400

    adds max profit solution

--- answers.py
+++ answers.py
@@ -850,3 +850,12 @@ class Solution:
             root.left = self.buildTree(preorder, inorder[:index])
             root.right = self.buildTree(preorder, inorder[index+1:])
             return root
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price, max_profit = float('inf'), 0
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price - min_price)
+        return max_profit
