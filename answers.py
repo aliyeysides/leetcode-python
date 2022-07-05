257449be75963260111ada7716ba8c06dfcc45d5
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-05 16:30:47 -0400

    adds coin change solution

--- answers.py
+++ answers.py
@@ -1,6 +1,7 @@
 from typing import Optional, List
 from collections import deque, defaultdict, Counter
 import sys
+from math import inf
 
 
 class TreeNode:
@@ -890,3 +891,14 @@ class Solution:
         if len(nums) <= 2:
             return max([0] + nums)
         return max(helper(nums[1:]), helper(nums[:-1]))
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [inf] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, range + 1):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i-coin], dp[i])
+        return dp[-1] if dp[-1] != inf else -1
