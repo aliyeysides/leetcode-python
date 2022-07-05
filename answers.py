0c3e9cea23c5c302284f25b4a68df594edd46a81
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-05 12:00:17 -0400

    adds build tree from inorder/preorder lists solution

--- answers.py
+++ answers.py
@@ -840,3 +840,13 @@ class Solution:
                 return False
 
         return dfs(p, q)
+
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if inorder:
+            index = inorder.index(preorder.pop(0))
+            root = TreeNode(inorder[index])
+            root.left = self.buildTree(preorder, inorder[:index])
+            root.right = self.buildTree(preorder, inorder[index+1:])
+            return root
