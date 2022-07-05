eeff3d5a06070c952fb9792ee9f8c3bb48258b84
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-05 10:05:10 -0400

    updates search k pivoted list solution

--- answers.py
+++ answers.py
@@ -136,24 +136,22 @@ class Solution:
         if not nums:
             return -1
 
-        low, high = 0, len(nums) - 1
-
-        while low <= high:
-            mid = (low + high) // 2
+        lower, upper = 0, len(nums) - 1
+        while lower <= upper:
+            mid = lower + (upper - lower) // 2
             if target == nums[mid]:
                 return mid
 
-            if nums[low] <= nums[mid]:
-                if nums[low] <= target <= nums[mid]:
-                    high = mid - 1
+            if nums[lower] <= nums[mid]:
+                if nums[lower] <= target <= nums[mid]:
+                    upper = mid
                 else:
-                    low = mid + 1
+                    lower = mid + 1
             else:
-                if nums[mid] <= target <= nums[high]:
-                    low = mid + 1
+                if nums[mid] <= target <= nums[upper]:
+                    lower = mid + 1
                 else:
-                    high = mid - 1
-
+                    upper = mid
         return -1
 
 
