3c358679cc30e732756ee4941520f8680567b38a
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-05 17:31:56 -0400

    adds unique paths dp solution

--- answers.py
+++ answers.py
@@ -481,6 +481,23 @@ class Solution:
 
 
 class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+
+        for r in range(m):
+            dp[r][0] = 1
+
+        for c in range(n):
+            dp[0][c] = 1
+
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+
+        return dp[-1][-1]
+
+
+class Solution:
     def map_gate_distances(dungeon_map: list[list[int]]) -> list[list[int]]:
         rows, cols = len(dungeon_map), len(dungeon_map[0])
         queue = deque()
