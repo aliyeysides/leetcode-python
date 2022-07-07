ee3ccc6fa45538bbc40329165d7487070c36dfb0
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-07 12:45:34 -0400

    adds longest unsorted consecutive sequence solution

--- answers.py
+++ answers.py
@@ -977,3 +977,22 @@ class Solution:
             record = max(record, dp[i])
 
         return record
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        record = 0
+
+        for num in nums:
+            if num - 1 not in num_set:
+                cur_num = num
+                cur_streak = 1
+
+                while cur_num + 1 in num_set:
+                    cur_streak += 1
+                    cur_num += 1
+
+                record = max(record, cur_streak)
+
+        return record
