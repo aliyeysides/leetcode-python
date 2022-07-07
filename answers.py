e75a15fa3723eb244a5148ee82124eadd7118110
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-07 16:42:50 -0400

    adds reverse linked list solution

--- answers.py
+++ answers.py
@@ -1043,3 +1043,15 @@ class Solution:
             memo[start_index] = ok
             return memo[start_index]
         return dfs(0, {})
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        p = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+
+        return p
