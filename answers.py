25fca0a8f72bde8aabc7f68998c7195b0c7dfa8f
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-07 13:49:07 -0400

    adds clone graph dfs solution

--- answers.py
+++ answers.py
@@ -4,6 +4,12 @@ import sys
 from math import inf
 
 
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -996,3 +1002,23 @@ class Solution:
                 record = max(record, cur_streak)
 
         return record
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        def dfs(root, visited):
+            if not root:
+                return root
+
+            if root in visited:
+                return visited[root]
+
+            clone = Node(root.val, [])
+            visited[root] = clone
+
+            if root.neighbors:
+                clone.neighbors = [dfs(nei, visited) for nei in root.neighbors]
+
+            return clone
+
+        return dfs(node, {})
