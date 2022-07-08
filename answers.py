40b9afcf3ec0e6361e698282b93ce46fa1477e37
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-08 17:30:47 -0400

    adds LCS dp solutions

--- answers.py
+++ answers.py
@@ -1113,3 +1113,35 @@ class Solution:
             return memo[(i, j)]
 
         return dfs(0, 0, {})
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0] * (len(text1) + 1) for _ in range(len(text2) + 1)]
+
+        for c in range(1, len(text1)+1):
+            for r in range(1, len(text2)+1):
+                if text1[c-1] == text2[r-1]:
+                    dp[r][c] = 1 + dp[r-1][c-1]
+                else:
+                    dp[r][c] = max(dp[r-1][c], dp[r][c-1])
+
+        return dp[-1][-1]
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        if text1 > text2:
+            text1, text2 = text2, text1
+
+        prev = [0] * (len(text1) + 1)
+        curr = [0] * (len(text1) + 1)
+
+        for c in range(1, len(text2)+1):
+            for r in range(1, len(text1)+1):
+                if text1[r-1] == text2[c-1]:
+                    curr[r] = 1 + prev[r-1]
+                else:
+                    curr[r] = max(prev[r], curr[r-1])
+            prev, curr = curr, prev
+        return prev[-1]
