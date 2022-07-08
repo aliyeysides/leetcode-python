44b49efa68647f31f08af15df9eb8843a045eea2
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-08 10:52:05 -0400

    adds iterative reverse linked list solution

--- answers.py
+++ answers.py
@@ -1058,6 +1058,18 @@ class Solution:
 
 
 class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+        while curr:
+            tmp = curr.next
+
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        return prev
+
+
+class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         slow = fast = head
         while fast and fast.next:
