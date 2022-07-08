9958ece4429d001fa6c40a6cb6d8536525e6dd35
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-08 15:15:45 -0400

    adds LCS dfs memo solution

--- answers.py
+++ answers.py
@@ -1092,3 +1092,24 @@ class Solution:
         while prev.next:
             head.next, head = prev, head.next
             prev.next, prev = head, prev.next
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m, n = len(text1), len(text2)
+
+        def dfs(i, j, memo):
+            if i == m or j == n:
+                return 0
+
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            if text1[i] == text2[j]:
+                memo[(i, j)] = 1 + dfs(i+1, j+1, memo)
+            else:
+                memo[(i, j)] = max(dfs(i+1, j, memo), dfs(i, j+1, memo))
+
+            return memo[(i, j)]
+
+        return dfs(0, 0, {})
