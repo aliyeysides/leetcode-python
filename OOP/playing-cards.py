f0c99c136d7811396733b87544cd9a5f473ce2f7
Ali Yeysides <ali.yeysides@gmail.com>
2022-07-17 13:21:02 -0400

    implements game and adds joker class

--- OOP/playing-cards.py
+++ OOP/playing-cards.py
@@ -49,30 +49,55 @@ class PlayingCard(Card):
         return f"{value} of {suit}"
 
 
+class Color(Enum):
+    RED = auto()
+    BLACK = auto()
+
+
+class JokerCard(Card):
+    COLORS = {
+        'Red': Color.RED,
+        'Black': Color.BLACK
+    }
+    COLOR_NAMES = {e: n for n, e in COLORS.items()}
+
+    def __init__(self, color: str):
+        super().__init__()
+        self.__color = self.COLORS[color]
+        self.__value = 14
+
+    @property
+    def card_value(self) -> int:
+        return self.__value
+
+    def __str__(self) -> str:
+        color = self.COLOR_NAMES[self.__color]
+        return f"{color} Joker"
+
+
 class Game:
     def __init__(self):
-        # Implement initializer here
-        pass
+        self.__deck: list[Card] = []
 
     def add_card(self, suit: str, value: str) -> None:
-        # Implement function here
-        pass
+        self.__deck.append(PlayingCard(suit, value))
 
     def card_string(self, card: int) -> str:
-        # Implement function here
-        return ""
+        return str(self.__deck[card])
 
     def card_beats(self, card_a: int, card_b: int) -> bool:
-        # Implement function here
-        return False
+        return self.__deck[card_a] > self.__deck[card_b]
+
+    def add_joker(self, color: str):
+        self.__deck.append(JokerCard(color))
 
 
 if __name__ == '__main__':
     game = Game()
     suit, value = input().split()
-    game.add_card(suit, value)
+    game.add_joker(value) if suit == "Joker" else game.add_card(suit, value)
     print(game.card_string(0))
     suit, value = input().split()
-    game.add_card(suit, value)
+    game.add_joker(value) if suit == "Joker" else game.add_card(suit, value)
     print(game.card_string(1))
     print("true" if game.card_beats(0, 1) else "false")
