c4b29053c669f88b2f46d270c7c29e20273bc03d
Ali Yeysides <ali.yeysides@gmail.com>
2022-08-25 16:42:01 -0400

    update

--- answers.py
+++ answers.py
@@ -1079,11 +1079,7 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev, curr = None, head
         while curr:
-            tmp = curr.next
-
-            curr.next = prev
-            prev = curr
-            curr = tmp
+            curr.next, prev, curr = prev, curr, curr.next
         return prev
 
 
